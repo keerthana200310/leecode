2cca7c0149fd5d44edfea560a061b6620dc6efba
keerthana200310 <1nc23cd024@ncetmail.com>
2026-03-14 09:59:38 +0530

    Time: 455 ms (67.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+SELECT 
+    product_id,
+    COALESCE(new_price, 10) AS price
+FROM (
+    SELECT 
+        product_id,
+        new_price,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id 
+            ORDER BY change_date DESC
+        ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+) t
+WHERE rn = 1
+
+UNION
+
+SELECT DISTINCT product_id, 10
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+);
